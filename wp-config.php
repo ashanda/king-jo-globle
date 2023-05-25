be4fabe09290b9c82fad0fee0c2f5d1b85d1c6a3
Sammani Jayaweera <sammanijayaweera0411@gmail.com>
2023-05-25 00:09:58 -0700

    server setup

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'king-jo' );
+define( 'DB_NAME', 'yogeedev_king-jo-globle' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'yogeedev_king-jo-globle' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'kk$,iJLd]O;G' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
