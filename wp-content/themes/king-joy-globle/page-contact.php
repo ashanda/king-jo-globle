c16391257e7319a4952085aff6fb65df70f3c42b
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2023-06-01 18:42:24 +0530

    responsive

--- wp-content/themes/king-joy-globle/page-contact.php
+++ wp-content/themes/king-joy-globle/page-contact.php
@@ -8,7 +8,7 @@ get_header();  ?>
 <div class="full-content">
 
     <!-- Start about-us section -->
-    <div class="contact-us-details py-5" style="background-image: url(<?php echo get_template_directory_uri(); ?>/inc/king-images/Group\ 8437.png);">
+    <div class="contact-us-details py-3 py-lg-5" style="background-image: url(<?php echo get_template_directory_uri(); ?>/inc/king-images/Group\ 8437.png);">
 
         <div class="cont container my-5">
             <div class="row">
@@ -16,7 +16,7 @@ get_header();  ?>
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.30721008618!2d79.99224377562258!3d7.09034271631545!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae2fbed59abfe0b%3A0x156c4d8153c22b78!2sYogee%20Media%20(Pvt)%20Ltd.!5e0!3m2!1sen!2slk!4v1685618110918!5m2!1sen!2slk" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <div class="col-md-6">
-                    <section class="mb-4">
+                    <section class="mb-4 mt-5 mt-md-0">
 
                         <!--Section heading-->
                         <h2 class="h1-responsive font-weight-bold text-center mb-4">Contact us</h2>
@@ -59,7 +59,7 @@ get_header();  ?>
                             </div>
                             <!--Grid column-->
 
-                            <div class="col-lg-3 text-center">
+                            <div class="col-lg-3 text-center mt-5 mt-lg-0">
                                 <ul class="list-unstyled mb-0">
                                     <li><i class="fas fa-map-marker-alt fa-2x"></i>
                                         <p>San Francisco, CA 94126, USA</p>
